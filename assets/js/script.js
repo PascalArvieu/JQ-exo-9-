4512af7b5ab35a014d18e62be4c5b7caed931b37
PascalArvieu <pascalarvieu@gmail.com>
2020-11-09 15:06:38 +0100

    JQ exo 9

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(function () {
+	$("#greenButton").click(function () {
+		$("#text").css("color","green")();
+	});
+
+	$("#blueButton").click(function () {
+		$("#text").css("color","blue")();
+	});
+
+	$("#redButton").click(function () {
+		$("#text").css("color","red")();
+	});
+});
